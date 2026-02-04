bd32e1490f21ebc794fb5c1f912b7fbe474572d5
Tommysepf <tsepulvedf@eafit.edu.co>
2026-02-04 13:19:08 -0500

    cambios en la funcion about

--- movie/views.py
+++ movie/views.py
@@ -6,3 +6,7 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name':'Tomás Sepúlveda'})
+
+def about(request):
+    #return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
